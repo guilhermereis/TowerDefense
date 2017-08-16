91a438fc130c464eefd6dc8b6b1ef85c209a8347
Guilherme Marques <guilherme.marques@inlocomedia.com>
2017-08-16 18:27:29 -0300

    added working grid and started coding grid selection on GridMouse.cs

---
+++ Assets/GridQuad.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GridQuad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnMouseEnter()
+    {
+        Debug.Log("Mouse entered !");
+    }
+    void OnMouseExit()
+    {
+        Debug.Log("Mouse left !");
+    }
+    void OnMouseDown()
+    {
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
