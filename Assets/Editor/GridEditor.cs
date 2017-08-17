0979d038815d22e02821624b481016cf282da0c2
Guilherme Marques <guilherme.marques@inlocomedia.com>
2017-08-17 11:20:06 -0300

    added WORKING grid, with selection and instantiation

---
+++ Assets/Editor/GridEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GridMouse))]
+public class GridEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GridMouse grid = target as GridMouse;
+
+        grid.UpdateGrid();
+    }
+
+}
