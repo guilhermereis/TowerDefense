dcfd7f68e140c04b8c288a9aead5ebf432f1d818
Anderson Luiz <alfr@cin.ufpe.br>
2017-08-21 22:09:29 -0300

    add catapult prototype

---
+++ Assets/CatapultController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatapultController : BuildableController {
+
+	private List<GameObject> enemies;
+	public GameObject target;
+	public GameObject bulletPrefab;
+	private Transform launchOrigin;
+	private float fireRate;
+	private float attackPower;
+	private float coolDown = 0;
+
+	public float AttackPower
+	{
+		get
+		{
+			return attackPower;
+		}
+
+		set
+		{
+			attackPower = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		fireRate = 0.4f;
+		AttackPower = 400f;
+		Health = 100f;
+		Defense = 5f;
+		IsUpgradable = true;
+		enemies = new List<GameObject>();
+		launchOrigin = transform.GetChild(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(coolDown <= 0 && enemies.Count > 0)
+		{
+			LaunchProjectile();
+			coolDown = 1 / fireRate;
+		}
+		coolDown -= Time.deltaTime;
+	}
+
+	private void LaunchProjectile()
+	{
+		Debug.Log(transform.rotation.eulerAngles);
+		GameObject projectile = Instantiate(bulletPrefab, launchOrigin.position, transform.rotation);
+		projectile.transform.parent = gameObject.transform;
+		projectile.transform.localRotation = Quaternion.identity;
+
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (!other.isTrigger && other.gameObject.tag == "Enemy")
+		{
+
+			enemies.Add(other.gameObject);
+			if (target == null)
+				target = other.gameObject;
+
+		}
+	}
+
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (!other.isTrigger  && other.gameObject.tag == "Enemy")
+		{
+			enemies.Remove(other.gameObject);
+			if (target == other.gameObject)
+				target = null;
+
+		}
+	}
+
+}
---
+++ Assets/FireBall.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour {
+
+	public float range;
+	private Rigidbody rig;
+	public float angle;
+	public float speed;
+	public GameObject explosionParticlePrefab;
+	private Vector3 dir;
+	// Use this for initialization
+	void Start () {
+		range = 20f;
+		rig = GetComponent<Rigidbody>();
+		dir = ((transform.position + transform.forward * range) - transform.position);
+		//Quaternion lookRotation = Quaternion.LookRotation(dir);
+
+		//Vector3 rotation = lookRotation.eulerAngles;
+		Debug.Log(transform.rotation.eulerAngles);
+		//transform.rotation = Quaternion.Euler(0f, 270, 0f);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	
+
+		rig.MovePosition(transform.position + dir.normalized * speed * Time.deltaTime);
+
+
+
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		
+		Instantiate(explosionParticlePrefab,collision.contacts[0].point, Quaternion.identity);
+		Destroy(gameObject);
+	}
+}
