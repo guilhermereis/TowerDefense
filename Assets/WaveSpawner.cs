27271602e43aee120c74f28753bb4fb75cb7ea3a
Anderson Luiz <alfr@cin.ufpe.br>
2017-08-28 22:54:35 -0300

    added spawn timer so the enemies can spawn with an interval of spawn timer seconds

--- Assets/WaveSpawner.cs
+++ Assets/WaveSpawner.cs
@@ -13,9 +13,11 @@ public class WaveSpawner : MonoBehaviour {
 
     public Transform spawnLocation;
 
-    
-
-
+    bool isWaving = false;
+    float spawnTimer = 2f;
+    float timer = 0;
+    int[] combination;
+    int spawningMonster = 0;
     private void Start()
     {
         //monsterBatch = new List<GameObject>();
@@ -36,31 +38,46 @@ public class WaveSpawner : MonoBehaviour {
             GameController.gc.ChangeGameState(GameController.GameState.EndWave);
         }
         
+        if(isWaving)
+        {
+            BeginSpawn();
+        }
+
     }
 
     void CreateWave()
     {
+
         currentWave = new Wave(waveNumber * 2, waveNumber);
+        combination = currentWave.GetCombinaton();
         waveNumber++;
-        BeginSpawn();
+        isWaving = true;
+       
     }
     
+    private IEnumerator WaitandSpawn(int monsterIndex)
+    {
+        yield return new WaitForSeconds(3);
+
+    }
 
 	void BeginSpawn()
 	{
-        int[] combination = currentWave.GetCombinaton();
-        for (int i = 0; i < combination.Length; i++)
+        
+        if(timer <= 0)
         {
-            int monsterIndex = combination[i] - 1;
-           
-
-            GameObject monster = Instantiate(monstersPrefab[monsterIndex], spawnLocation.position,Quaternion.identity);
+            int monsterIndex = combination[spawningMonster] - 1;
+            GameObject monster = Instantiate(monstersPrefab[monsterIndex], spawnLocation.position, Quaternion.identity);
             monsterBatch.Add(monster);
             monster.transform.parent = transform;
+            spawningMonster++;
+            timer = spawnTimer;
+
         }
+        if (spawningMonster >= combination.Length)
+            isWaving = false;
 
-       
-
+        timer -= Time.deltaTime;
 
     }
 }
