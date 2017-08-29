e678e8554d6c70ed8a81d0595ef08a333611f8b4
Guilherme Marques <guilherme.marques@inlocomedia.com>
2017-08-28 23:12:29 -0300

    added temporary menu on canvas for debugging (DestroyAll button)

---
+++ Assets/DebugOptions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugOptions : MonoBehaviour {
+
+
+    public void doDestroyAll()
+    {
+        Debug.Log("Gonna destroy all !");
+    }
+}
