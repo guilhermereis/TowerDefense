757837e0dbcc6ff19372009a58b03d4288137d5d
luizsotero <fernandosotero@outlook.com>
2017-08-31 23:38:39 -0300

    Added Gobling Wanderer with its animations and controller for now

---
+++ Assets/Pawns/Scripts/WandererAnimatorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandererAnimatorController : MonoBehaviour {
+    public Animator animInstance;
+
+    public bool isAttacking = false;
+    public float speed = 0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (animInstance)
+        {
+            animInstance.SetBool("IsAttacking", isAttacking);
+            animInstance.SetFloat("Speed", speed / 4f);
+        }
+    }
+}
