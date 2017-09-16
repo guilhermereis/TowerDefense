10c8e770fcdcd8ff3668847777fea00db0cd79d4
Guilherme Marques <guilherme.marques@inlocomedia.com>
2017-09-16 10:47:18 -0300

    added menu and logic to upgrade to TowerSlow and TowerTesla

---
+++ Assets/OptionsSlow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsSlow : Options {
+
+    public void UpgradeSlow()
+    {
+
+    }
+    public void UpgradeTesla()
+    {
+
+    }
+}
