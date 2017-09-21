50485a005a305ed48484d1047fe78b354e980edd
Guilherme Marques <guilherme.marques@inlocomedia.com>
2017-09-20 23:06:15 -0300

    made minimap work

--- Assets/Minimap.cs
+++ Assets/Minimap.cs
@@ -4,25 +4,35 @@ using UnityEngine;
 
 public class Minimap : MonoBehaviour {
 
-    public GameObject square; 
+    public GameObject square;
+    public GameObject diagonal_grid;
+    public GameObject monster;
 	// Use this for initialization
 	void Start () {
         UpdateMap();
     }
     public void UpdateMap()
     {
+        //Camera.main.WorldToScreenPoint()
         Debug.Log("GOING TO MOVE " + square);
-        Matrix4x4 matrix = square.GetComponent<RectTransform>().worldToLocalMatrix;
+        Vector2 centerOfMinimap = new Vector2(790 / 2 + 320, 369 / 2 + 60);
+        square.GetComponent<RectTransform>().position = centerOfMinimap;
+        float diagonal_minimap = this.GetComponent<RectTransform>().rect.width;
+        float scale = diagonal_grid.GetComponent<Renderer>().bounds.size.x / diagonal_minimap;
+        Debug.Log("Scale = " + scale);
 
-        square.GetComponent<RectTransform>().localScale = this.transform.localScale;
-        square.GetComponent<RectTransform>().position = matrix.MultiplyPoint(new Vector3(0, 0, 0));
-        Debug.Log("Set!!!!");
+
+        Vector3 vector = monster.transform.position * scale;
+        Vector2 new_vector = new Vector2(vector.x, vector.z);
+        square.transform.position = centerOfMinimap + new_vector;
+
+
     }
 
 
 	// Update is called once per frame
 	void Update () {
         //square.transform.position;
-        	
+        UpdateMap();
 	}
 }
