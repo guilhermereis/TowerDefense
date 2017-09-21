19b2451bbb17c83c292b95c4bc49af1a68dad5c9
luizsotero <fernandosotero@outlook.com>
2017-09-21 01:00:34 -0300

    Added Tower infos on info bar

--- Assets/UI/Scripts/BottomInfoBarBehaviour.cs
+++ Assets/UI/Scripts/BottomInfoBarBehaviour.cs
@@ -1,13 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BottomInfoBarBehaviour : MonoBehaviour {
 
     public float slideSpeed = 5f;
     public float closedPosition = -50f;
     public float openPosition = 0f;
+    private GameObject selectedObject;
     private int selectionState = 0; //0 = Nothing selected, 1 = Has selection;
+    public GameObject AttackSpeedText;
+    public GameObject AttackSpeedTextShadow;
+    public GameObject DamageText;
+    public GameObject DamageTextShadow;
+    public GameObject SelectionName;
+    public GameObject SelectionNameShadow;
+
+    public void setSelectedUnit(GameObject selectedObject) {
+        TowerController controller = selectedObject.GetComponent<TowerController>();
+        if (controller)
+        {
+            AttackSpeedText.GetComponent<Text>().text = "" + controller.getFireRate();
+            AttackSpeedTextShadow.GetComponent<Text>().text = "" + controller.getFireRate();
+
+            DamageText.GetComponent<Text>().text = "" + controller.getAttackPower();
+            DamageTextShadow.GetComponent<Text>().text = "" + controller.getAttackPower();
+
+            switch (controller.name) {
+                case "PrefabArcherTower1(Clone)":
+                    SelectionName.GetComponent<Text>().text = "Tower Lvl1: ";
+                    SelectionNameShadow.GetComponent<Text>().text = "Tower Lvl1: ";
+                    break;
+                case "PrefabArcherTower2(Clone)":
+                    SelectionName.GetComponent<Text>().text = "Tower Lvl2: ";
+                    SelectionNameShadow.GetComponent<Text>().text = "Tower Lvl2: ";
+                    break;
+                case "PrefabArcherTower3(Clone)":
+                    SelectionName.GetComponent<Text>().text = "Tower Lvl3: ";
+                    SelectionNameShadow.GetComponent<Text>().text = "Tower Lvl3: ";
+                    break;
+                case "PrefabArcherTower2Slow(Clone)":
+                    SelectionName.GetComponent<Text>().text = "Icer Tower: ";
+                    SelectionNameShadow.GetComponent<Text>().text = "Icer Tower: ";
+                    break;
+                case "PrefabArcherTower2Tesla(Clone)":
+                    SelectionName.GetComponent<Text>().text = "Tesla Tower: ";
+                    SelectionNameShadow.GetComponent<Text>().text = "Tesla Tower: ";
+                    break;
+            }
+        }
+        else {
+            //Get Camp Controller
+        }
+
+    }
+
+    public GameObject getSelectedUnit() {
+        return selectedObject;
+    }
 
     public void setSelectionState(int SelectionState) {
         this.selectionState = SelectionState % 2;
