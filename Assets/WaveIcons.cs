4eae69d9915270e00adbc666ee678dd1d52a2c1c
Guilherme Marques <guilherme.marques@inlocomedia.com>
2017-09-22 15:50:44 -0300

    disabled WaveIcons UI

--- Assets/WaveIcons.cs
+++ Assets/WaveIcons.cs
@@ -14,14 +14,25 @@ public class WaveIcons : MonoBehaviour {
         //allIcons[allIcons.Count - 1].transform.parent = transform;
         GameObject _icon = Instantiate(icon, Vector2.zero, Quaternion.identity);
         _icon.transform.parent = transform;
+        allIcons.Add(_icon);
 
     }
+    public void removeIcon(int index)
+    {
+        allIcons[index].SetActive(false);
+    }
+    /*
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.P))
         {
             addNewIcon();
         }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            removeIcon(2);
+        }
     }
-
+    */
 }
