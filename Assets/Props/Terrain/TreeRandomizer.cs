18bae1028d2598e37925ac7d9aa60f5e0071e7a3
luizsotero <fernandosotero@outlook.com>
2017-09-23 12:51:21 -0300

    Added Snow Biome

--- Assets/Props/Terrain/TreeRandomizer.cs
+++ Assets/Props/Terrain/TreeRandomizer.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class TreeRandomizer : MonoBehaviour {
     public Material[] MVariation;
+    public Material[] SnowMVariation;
     public Vector3 initialScale;
+    public Theme theme;
+
+    public enum Theme {Grassland, Snow};
+
 #if UNITY_EDITOR
     // Use this for initialization
     void Start () {
@@ -13,7 +18,18 @@ public class TreeRandomizer : MonoBehaviour {
             float xyRandom = Random.Range(-0.002f, 0.002f);
             transform.localScale = initialScale + new Vector3(xyRandom, xyRandom, Random.Range(-0.002f, 0.002f));
             transform.Rotate(new Vector3(0f, 0f, Random.Range(0f, 360f)));
-            GetComponent<Renderer>().material = MVariation[(int)Random.Range(0, MVariation.Length)];
+
+            switch (theme) {
+                case Theme.Grassland:
+                    if(MVariation.Length>0)
+                    GetComponent<Renderer>().material = MVariation[(int)Random.Range(0, MVariation.Length)];
+                    break;
+                case Theme.Snow:
+                    if (SnowMVariation.Length > 0)
+                        GetComponent<Renderer>().material = SnowMVariation[(int)Random.Range(0, SnowMVariation.Length)];
+                    break;
+            }
+            
         }
     }
 
