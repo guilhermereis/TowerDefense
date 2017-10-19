0670f7ee289d9dc54c07504665ae66305d375d05
Guilherme Marques <guilherme.marques@inlocomedia.com>
2017-10-19 16:59:21 -0300

    gave a little nudge on the selection cube so it fits perfectly inside the grid's squares

--- Assets/GridMouse.cs
+++ Assets/GridMouse.cs
@@ -708,7 +708,7 @@ public class GridMouse : MonoBehaviour
             position = CoordToPosition(x, z);
 
 
-            Vector3 positionCube = new Vector3(position.x, position.y + 0.1f, position.z);
+            Vector3 positionCube = new Vector3(position.x+0.1f, position.y + 0.1f, position.z - 0.1f);
             selectionCube.transform.position = positionCube;
             //Debug.Log("TILE: " + x + "," + z + " OF TYPE: " + propertiesMatrix[x, z].type);
 
