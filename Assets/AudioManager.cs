322abadd044f3b02f29bacbe5f427d660188a35c
luizsotero <fernandosotero@outlook.com>
2017-10-23 16:37:30 -0300

    Coding the Config Screen, Audio Part with Master volume and BG musics done

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioSource snowlandsAmbientSound;
+    public AudioSource grasslandsAmbientSound;
+    public AudioSource desertAmbientSound;
+    public AudioSource vulcanoLandAmbientSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        SoundToPlay.PlayMusic(snowlandsAmbientSound);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
