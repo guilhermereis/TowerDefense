89c7a3a6792e5efda01242fd44af9b00e3dfab9c
luizsotero <fernandosotero@outlook.com>
2017-10-24 18:13:34 -0300

    Main Menu Done

--- Assets/UI/Scripts/ConfigurationMenu.cs
+++ Assets/UI/Scripts/ConfigurationMenu.cs
@@ -35,7 +35,6 @@ public class ConfigurationMenu : MonoBehaviour {
 
     void Awake()
     {
-        DontDestroyOnLoad(gameObject);
         soundSliderDelegate += processSliderChange;
         ResolutionConfirmationScreenController.countDownFinished += onScreenResolutionCountDownFinished;
     }
---
+++ Assets/UI/Scripts/MainMenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    public GameObject configMenu;
+    public AudioSource mainMenuBG;
+
+    private void Start()
+    {
+        SoundToPlay.PlayMusic(mainMenuBG);
+    }
+    public void StartGame() {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void ShowConfigScreen() {
+        if(configMenu)
+            configMenu.GetComponent<ConfigurationMenu>().Show();
+    }
+
+    public void HideConfigScreen() {
+        if (configMenu)
+            configMenu.GetComponent<ConfigurationMenu>().Hide();
+    }
+}
