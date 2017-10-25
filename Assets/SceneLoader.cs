5f80dca9bfae5bd7f841405930a0677e96ca4948
Anderson Luiz <alfr@cin.ufpe.br>
2017-10-25 15:47:42 -0300

    added scene loader

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+
+    private bool loadScene = false;
+    public int scene;
+    public Text tipText;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(LoadNewScene());
+	}
+	
+    IEnumerator LoadNewScene()
+    {
+
+        yield return new WaitForSeconds(3);
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(scene);
+        while (!async.isDone)
+        {
+            tipText.color = Random.ColorHSV();//new Color(tipText.color.r, tipText.color.g, tipText.color.b, Mathf.PingPong(Time.time, 1));
+            yield return null;
+        }
+    }
+
+
+	
+}
---
+++ Assets/TipText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TipText : MonoBehaviour {
+
+    public string[] tips = { " Build mine to make money", " Ice towers slow enemies" };
+    private Text tipText;
+	// Use this for initialization
+	void Start () {
+        tipText = GetComponent<Text>();
+        tipText.text += tips[Random.Range(0, tips.Length)];
+	}
+	
+	
+}
