0dbe3a409b713add76cbae7ae8316f1ec615d170
Guilherme Marques <guilherme.marques@inlocomedia.com>
2017-10-27 12:50:37 -0300

    changed clicksound script to use sfx volume

--- Assets/ClickSound.cs
+++ Assets/ClickSound.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Button))]
@@ -24,6 +22,7 @@ public class ClickSound : MonoBehaviour {
 
     void PlaySound()
     {
-        source.PlayOneShot(sound);
+        SoundToPlay.PlaySfx(source);
+        //source.PlayOneShot();
     }
 }
